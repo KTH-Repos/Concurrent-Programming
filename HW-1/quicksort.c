baeaecdeec3260a0bcf38dcc3008870106b9f5d9
Tomas <you@example.com>
2023-02-05 16:19:39 +0100

    update quicksort again

--- HW-1/quicksort.c
+++ HW-1/quicksort.c
@@ -1,44 +1,22 @@
-#ifndef _REENTRANT 
-#define _REENTRANT 
-#endif 
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <time.h>
 #include <sys/time.h>
-#define MAXSIZE 1000  /* maximum matrix size */
+#define MAXSIZE 1000000   /* maximum list size */
 #define MAXWORKERS 10   /* maximum number of workers */
 
-pthread_mutex_t barrier;  /* mutex lock for the barrier */
-pthread_cond_t go;        /* condition variable for leaving */
-int numWorkers;           /* number of workers */ 
-int numArrived = 0;       /* number who have arrived */
+pthread_mutex_t lock;
+int activeWorkers = 0;
+int list[MAXSIZE];
+struct sublist_data {
+    int *sublist;
+    int start;
+    int end;
+};
 
-double start_time, end_time; /* start and end times */
-int size, stripSize;  /* assume size is multiple of numWorkers */
-int sums[MAXWORKERS]; /* partial sums */
-int matrix[MAXSIZE][MAXSIZE]; /* matrix */
-
-int maxNum, maxNumRow, maxNumCol;
-
-int minNumRow, minNumCol, minNum;
-
-int globalSum;
-
-int bag_of_tasks_counter = 0;
-
-/* a reusable counter barrier */
-void Barrier() {
-  pthread_mutex_lock(&barrier);
-  numArrived++;
-  if (numArrived == numWorkers) {
-    numArrived = 0;
-    pthread_cond_broadcast(&go);
-  } else
-    pthread_cond_wait(&go, &barrier);
-  pthread_mutex_unlock(&barrier);
-}
+void task_coordinator(int *sublist, int start, int end);
 
 /* timer */
 double read_timer() {
@@ -54,169 +32,109 @@ double read_timer() {
     return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
 }
 
-void *Worker(void *);
-
-int getCounter(void) {
-    pthread_mutex_lock(&barrier);
-    int currentRow = bag_of_tasks_counter;
-    bag_of_tasks_counter++;
-    pthread_mutex_unlock(&barrier);
-    return currentRow;
+//swap function 
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-void findMaxForGlobal(int first) {
-  int max, maxRow, maxCol = 0;
-  int i, j;
-  i = first;
-  for(j = 0; j < size; j++) {
-    if(matrix[i][j] > max) {
-      max = matrix[i][j];
-      maxRow = i+1;
-      maxCol = j+1;
+int partition(int *sublist, int start, int end, int pivot) {
+    
+    int pivotElem =sublist[pivot];
+    swap(&sublist[pivot], &sublist[end]);
+    int startIndex = start;
+
+    for(int i = start; i < end; i++) {
+        if(sublist[i] < pivotElem){
+            swap(&sublist[i], &sublist[startIndex]);
+            startIndex++;
+        }
     }
-  }
-  
-  pthread_mutex_lock(&barrier);
-  if(max > maxNum) {
-      maxNum = max;
-      maxNumRow = maxRow;
-      maxNumCol = maxCol;
-      pthread_mutex_unlock(&barrier);
-  }
-  else{
-    pthread_mutex_unlock(&barrier);
-  }
+    swap(&sublist[startIndex], &sublist[end]);
+    return startIndex;
 }
 
-void findMinForGlobal(int first) {
-  int minRow, minCol, min = 0;
-  int i, j;
-  i = first;
-  for(j = 0; j < size; j++) {
-    if(matrix[i][j] < min) {
-      min = matrix[i][j];
-      minRow = i+1;
-      minCol = j+1;
+void quickSort_sequential(int *sublist, int start, int end) {
+    if(start < end) {
+        int pivot = start + (end-start)/2;
+        int pivotPos = partition(sublist, start, end, pivot);
+
+        quickSort_sequential(sublist, start, pivotPos-1);
+        quickSort_sequential(sublist, pivotPos+1, end);
     }
-  }
-  
-  pthread_mutex_lock(&barrier);
-  if(min < minNum) {
-    minNum = min;
-    minNumRow = minRow;
-    minNumCol = minCol;
-    pthread_mutex_unlock(&barrier);
-  }
-  else{
-    pthread_mutex_unlock(&barrier);
-  }
 }
 
-int main(int argc, char *argv[]) {
-  int i, j;
-  long l; /* use long in case of a 64-bit system */
-  pthread_attr_t attr;
-  pthread_t workerid[MAXWORKERS];
+void *quickSort_parallel(void *sublist_data) {
+    struct sublist_data *data = (struct sublist_data*) sublist_data;
+    task_coordinator(data->sublist, data->start, data->end);
+} 
 
-  /* set global thread attributes */
-  pthread_attr_init(&attr);
-  pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+void task_coordinator(int *sublist, int start, int end) {
 
-  /* initialize mutex and condition variable */
-  pthread_mutex_init(&barrier, NULL);
-  pthread_cond_init(&go, NULL);
+    if(start < end) {
+        if(end-start>100) {
 
-  /* read command line args if any */
-  size = (argc > 1)? atoi(argv[1]) : MAXSIZE;
-  numWorkers = (argc > 2)? atoi(argv[2]) : MAXWORKERS;
-  if (size > MAXSIZE) size = MAXSIZE;
-  if (numWorkers > MAXWORKERS) numWorkers = MAXWORKERS;
-  stripSize = size/numWorkers;
-  //printf("The strip is: %d", stripSize);  
-  printf("\n"); 
+            int pivot = start + (end-start)/2;
+            int pivotPos = partition(sublist, start, end, pivot);
 
-  /* initialize the matrix */
-  for (i = 0; i < size; i++) {
-	  for (j = 0; j < size; j++) {
-          matrix[i][j] = rand()%99;
-	  }
-  }
+            pthread_mutex_lock(&lock);
+            if(activeWorkers < MAXWORKERS) {
+                activeWorkers++;
+                pthread_mutex_unlock(&lock); 
 
-  minNum = matrix[MAXSIZE-1][MAXSIZE-1];
-  minNumCol = MAXSIZE-1;
-  minNumRow = MAXSIZE-1;
+                pthread_t thread;
+                struct sublist_data sublist_sub = {sublist, start, pivotPos-1};
+                pthread_create(&thread, NULL, quickSort_parallel, (void *) &sublist_sub);
+                
+                printf("(pthread id %ld) has started\n", pthread_self());
+                task_coordinator(sublist, pivotPos+1, end);
 
-  maxNum = 0;
-  maxNumCol = 0;
-  maxNumRow = 0;
-
-  /* print the matrix */
-  #ifdef DEBUG
-  for (i = 0; i < size; i++) {
-	  printf("[ ");
-	  for (j = 0; j < size; j++) {
-	    printf(" %d", matrix[i][j]);
-	  }
-	  printf(" ]\n");
-  }
-  #endif
-
-  /* do the parallel work: create the workers */
-  
-  start_time = read_timer();
-  for (l = 0; l < numWorkers; l++)
-    pthread_create(&workerid[l], &attr, Worker, (void *) l);
-
-  for (l = 0; l < numWorkers; l++)
-     pthread_join(workerid[l], NULL);
-
-  printf("This is printed by main thread\n");
-  printf("Min number in matrix is %d, located at [%d][%d]\n", minNum, minNumRow, minNumCol);
-  printf("Max number in matrix is %d, located at [%d][%d]\n", maxNum, maxNumRow, maxNumCol);
-  pthread_exit(NULL);
-}
-
-/* Each worker sums the values in one strip of the matrix.
-   After a barrier, worker(0) computes and prints the total */
-
-void *Worker(void *arg) {
-    int currentRow = 0;
-    long myid = (long) arg;
-    int total, i, j, first, last;
-
-    #ifdef DEBUG
-    printf("worker %ld (pthread id %ld) has started\n", myid, pthread_self());
-    #endif
-
-    while(currentRow < size) {
-
-      currentRow = getCounter();
-
-      if(currentRow >= size)
-        break;
-
-      first = currentRow;
-      last = currentRow;
-
-      findMinForGlobal(first);
-      findMaxForGlobal(first);
-
-      total = 0;
-      for (i = first; i <= last; i++)
-          for (j = 0; j < size; j++)
-              total += matrix[i][j];
-      sums[myid] += total;
-
+                pthread_join(thread, NULL);
+            }
+            else {
+                pthread_mutex_unlock(&lock);
+                quickSort_sequential(sublist, start, end);
+            } 
+        }
+        else{
+            //printf("sublist is too small to be sorted by new thread\n");
+            quickSort_sequential(sublist, start, end);
+        }
     }
-    Barrier();
-    if (myid == 0) {
-    total = 0;
-    for (i = 0; i < numWorkers; i++)
-      total += sums[i];
-    /* get end time */
-    end_time = read_timer();
-    /* print results */
-    printf("The total is %d\n", total);
-    printf("The execution time is %g sec\n", end_time - start_time);
-  }
+}
+
+void initList(int *list) {
+    int i;
+    for(i = 0; i < MAXSIZE; i++) {
+        list[i] = rand()%9999;
+    }
+}
+
+void printList(int *list) {
+    int i;
+    for(i = 0; i < MAXSIZE; i++) {
+        printf("%d ", list[i]);
+    }
+}
+
+int main(int argc, char*argv []) {
+    initList(list);
+
+    //printList(list);
+
+    double starttime = read_timer();
+
+    task_coordinator(list, 0, MAXSIZE-1);
+
+    double finishtime = read_timer();
+
+    //printf("\n");
+    //printList(list);
+
+
+    printf("Time taken by algo : %g sec", finishtime-starttime);
+
+    return 0;
+
 }
