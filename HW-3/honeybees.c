1d1283be2776d7477cc68d7f83a90be9fc3fa6ba
Tomas <73959718+tywe98@users.noreply.github.com>
2023-02-17 16:59:26 +0100

    add comments & remove mutex lock

--- HW-3/honeybees.c
+++ HW-3/honeybees.c
@@ -7,53 +7,50 @@
 #define PRODUCERS 3
 #define HONEYPOT_CAPACITY 5
 
-//n honeybees, one hungry bear, honeypot with capacity H
+//N honeybees, one hungry bear, honeypot with capacity H
 
-pthread_t bees[PRODUCERS];
+pthread_t bees[PRODUCERS];      //N honeybees
 pthread_t bear;
-int honeypot_portions;
+int honeypot_portions;          //symbolized honeypot, accessed by honeybees and bear
 
-sem_t full, empty;
-sem_t mutexC;
+sem_t full, empty;              //semaphores for mutex lock and conditional synchronization 
 
+
+/* FUnction executed by honeybees */
 void *producing(void *arg) {
     long myID = (long) arg;
     while(true) {
-        sem_wait(&empty);
-        sem_wait(&mutexC);
-        honeypot_portions++;
+        sem_wait(&empty);       //P(empty) - honeypot is empty and bees must fill it in until its full
+        honeypot_portions++;    //fill in honeypot with one portion
         if(honeypot_portions == HONEYPOT_CAPACITY) {
             printf("bee %ld made honey pot full, about to wake up bear ...\n\n", myID);
-            sem_post(&mutexC);
-            sem_post(&full);
+            sem_post(&full);    //bee who filled the last portion signals the bear
         }
         else {
-            printf("bee %ld filled one portion, currently %d portions in honey pot\n\n", myID, honeypot_portions);
-            sem_post(&mutexC);
-            sem_post(&empty);
-            sleep(3);
+            printf("bee %ld filled with one portion, currently %d portions in honey pot\n\n", myID, honeypot_portions);
+            sem_post(&empty);   //V(empty) - bee signals the others to fill in honeypot with portion until its full
+            sleep(2);           //bee sleeps for two secs
         }
     }
 }
 
+/* Function executed by bear */
 void *consuming(void *arg) {
     while(true) {
-        sem_wait(&full);
+        sem_wait(&full);        //P(full) - honeypot is full and bear wakes up to eat 
         honeypot_portions = 0;
         printf("the bear ate all honey portions, 0 portions left\n\n");
-        sem_post(&empty);
+        sem_post(&empty);       //V(empty) - honeypot is empty and bees can start filling in with portions
     }
 }
 
 
 int main(int argc, char*argv[]) {
 
-    int numbees = PRODUCERS;
-    honeypot_portions = 0;      
-
-    sem_init(&empty, true, 1);
-    sem_init(&full, true, 0);
-    sem_init(&mutexC, true, 1);     //used as mutex lock
+    int numbees = PRODUCERS;        //number of bees
+    honeypot_portions = 0;          //number of portions in honeypot is 0 from start   
+    sem_init(&empty, true, 1);      //semaphore used by bees as mutexlock & signaling mechanism between bees and from bear to producing bees
+    sem_init(&full, true, 0);       //semaphore used by bees to signal bear
 
     pthread_attr_t attr;
     pthread_attr_init(&attr);
--- HW-3/hungrybirds.c
+++ HW-3/hungrybirds.c
@@ -7,52 +7,49 @@
 #define CONSUMERS 3
 #define WORMS 5
 
-//n baby birds(CONSUMERS), one parent bird(PRODUCER), W worms
+//N baby birds(CONSUMERS), one parent bird(PRODUCER), W worms
 
-pthread_t babybirds[CONSUMERS];
+pthread_t babybirds[CONSUMERS];     //N baby birds
 pthread_t parent;
-int availableWorms;
+int availableWorms;                 //symbolizes dish with worms, accessed by babybirds and parent
 
-sem_t full, empty;
-sem_t mutexC;
+sem_t full, empty;                  //semaphores for mutex lock and conditional synchronization 
 
+
+/* Function executed by parent */
 void *producing(void *arg) {
     while(true) {
-        sem_wait(&empty);   //P(empty)
+        sem_wait(&empty);   //P(empty) - dish is empty and parent must fill in with worms.
         availableWorms = WORMS;
         printf("parent just filled in the dish with %d worms\n\n", availableWorms);
-        sem_post(&full);
+        sem_post(&full);    //V(full) - dish is full and babybirds can start eating
     }
 }
 
+/* Function executed by babybirds */
 void *consuming(void *arg) {
-    long myID = (long) arg;
+    long myID = (long) arg;     //id for babybird
     while(true) {
-        sem_wait(&full);
-        sem_wait(&mutexC);   //lock critical section
+        sem_wait(&full);    //P(full) - babybird wants to eat worm. If no worms avaiable then wait until parent bird fills dish and signal it.
         if(availableWorms > 0) {
-            availableWorms--;
+            availableWorms--;   //babybird takes one worm. This is critical section guarded by the semaphore full
             printf("babybird %ld ate one worm and there are %d worms left\n\n", myID, availableWorms);
-            sem_post(&mutexC);      //unlock critical section
-            sem_post(&full);
-            //printf("babybird %ld is going to sleep now\n\n", myID);
-            sleep(2);
+            sem_post(&full); //V(full) - babybird signals for others to come in and take worm
+            sleep(2); //babybird sleeps for two secs 
         }
         else {
             printf("babybird %ld found empty dish, waiting for parent ...\n\n", myID);
-            sem_post(&mutexC);
-            sem_post(&empty);
+            sem_post(&empty);   //babybird found no worm to take and signals to parent to fill in dish
         }
     }
 }
 
 int main(int argc, char*argv[]) {
 
-    int numbabybirds = CONSUMERS;
-    availableWorms = WORMS;
-    sem_init(&empty, true, 0);
-    sem_init(&full, true, 1);
-    sem_init(&mutexC, false, 1);    //used as mutex lock
+    int numbabybirds = CONSUMERS;       //number of babybirds
+    availableWorms = WORMS;             //number of worms is set to W from start
+    sem_init(&empty, true, 0);          //semaphore used by babybirds to signal parent
+    sem_init(&full, true, 1);           //semahore used by babybirds as mutexlock & signaling mechanism between babybirds and from parentbird to waiting babybirds
 
     pthread_attr_t attr;
     pthread_attr_init(&attr);
